5c596ce0bb1ef34c3f80f024fa83a3c318c32805
MrKtosiek <jezierski.filip@wp.pl>
2025-04-26 19:50:04 +0200

    Add some styling to login form

--- frontend/contacts-app/src/components/Header.tsx
+++ frontend/contacts-app/src/components/Header.tsx
@@ -1,15 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
 import { LOGIN_ROUTE, MAIN_MENU_ROUTE } from "../Constants";
+import "../styles/Header.scss";
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <nav>
-        <a onClick={() => navigate(MAIN_MENU_ROUTE)}><h1>Contacts App</h1></a>
-        <a onClick={() => navigate(LOGIN_ROUTE)}><h1>Login</h1></a>
+      <nav className="header">
+        <a onClick={() => navigate(MAIN_MENU_ROUTE)} className="title">Contacts App</a>
+        <a onClick={() => navigate(LOGIN_ROUTE)} className="login-button">Login</a>
       </nav>
     </>
   );
--- frontend/contacts-app/src/pages/Login.tsx
+++ frontend/contacts-app/src/pages/Login.tsx
@@ -1,11 +1,21 @@
 import { Header } from "../components/Header";
+import "../styles/Login.scss";
 
 export const Login: React.FC = () => {
-
   return (
     <>
       <Header />
-      TODO: Login form
+      <div className="login-background">
+        <form className="login-form">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" required />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" required />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 };
